d62c134f7021dceb7107490d0a7c80b44200865e
Félix VE <felive4@gmail.com>
2023-12-20 09:08:29 +0100

    resolve bug merge

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,12 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-<<<<<<< 445f51281dfcaf9a2868c38d8bbcf74c353610e0
 using UnityEngine.XR.Interaction.Toolkit;
-=======
-using static UnityEditor.Experimental.GraphView.GraphView;
-using UnityEngine.SceneManagement;
->>>>>>> 82b39237abbe714447c3aec94fb139f502a1dd4e
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -17,15 +12,12 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private float runSpeed;
 
-<<<<<<< 445f51281dfcaf9a2868c38d8bbcf74c353610e0
     [SerializeField] private ContinuousMoveProviderBase movement;
 
     private Vector2 _movement;
     private float _initial_speed;
     private bool _is_running = false;
-=======
 
->>>>>>> 82b39237abbe714447c3aec94fb139f502a1dd4e
     private void Start()
     {
         _initial_speed = movement.moveSpeed;
