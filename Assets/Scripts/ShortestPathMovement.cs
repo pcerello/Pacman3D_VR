dbcff5c387dedb7441601b20e9b0ce44ad699bac
Tanguy Decabrat <103762138+TDecabrat@users.noreply.github.com>
2024-01-09 12:04:37 +0100

    feat: Added move interval for shortestpathai, tried a fix for minimap in hand, fixed animations on ghosts

--- Assets/Scripts/ShortestPathMovement.cs
+++ Assets/Scripts/ShortestPathMovement.cs
@@ -2,17 +2,20 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI; //important
+using UnityEngine.AI;
 
-//if you use this code you are contractually obligated to like the YT video
-public class ShortestPathMovement : MonoBehaviour //don't forget to change the script name if you haven't
+public class ShortestPathMovement : MonoBehaviour
 {
     private Transform target;
     public NavMeshAgent Agent;
+    private float lastActionTime;
+    private bool paused;
 
     private void Start()
     {
         target = ScriptGameManager.SGM.GetTransformPlayer();
+        lastActionTime = -1;
+        paused = false;
     }
 
     void Update()
@@ -20,13 +23,30 @@ public class ShortestPathMovement : MonoBehaviour //don't forget to change the s
         Transform closestTarget = null;
         float closestTargetDistance = float.MaxValue;
         NavMeshPath Path = new NavMeshPath();
+        Debug.Log(paused);
 
         if (target == null)
         {
             Debug.Log("Either this is an error, either the player is not in the same navmesh. Probably must never happen.");
         }
+        else if (lastActionTime == -1)
+        {
+            lastActionTime = Time.time;
+        }
+        else if (Time.time - lastActionTime >= 0.5 && paused)
+        {
+            paused = false;
+            lastActionTime = Time.time;
+            Agent.isStopped = false;
+        }
+        else if (Time.time - lastActionTime >= 2.25 && !paused)
+        {
+            paused = true;
+            lastActionTime = Time.time;
+            Agent.isStopped = true;
+        }
 
-        if (NavMesh.CalculatePath(transform.position, target.position, Agent.areaMask, Path))
+        if (NavMesh.CalculatePath(transform.position, target.position, Agent.areaMask, Path) && !paused)
         {
             float distance = Vector3.Distance(transform.position, Path.corners[0]);
             for (int j = 1; j < Path.corners.Length; j++)
